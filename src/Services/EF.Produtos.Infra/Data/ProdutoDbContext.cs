1cfc4e8f41ee03d1301e109620a11a87c0c4b08d
Pedro Barão <pedrobarao@users.noreply.github.com>
2024-01-12 18:04:43 -0300

    feat: produtos (#21)
    Co-authored-by: Lucas Fernandes Lima <lucas.liima030@gmail.com>

---
+++ src/Services/EF.Produtos.Infra/Data/ProdutoDbContext.cs
@@ -0,0 +1,37 @@
+﻿using EF.Domain.Commons.Messages;
+using EF.Domain.Commons.Repository;
+using EF.Produtos.Domain.Models;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Produtos.Infra.Data;
+
+public sealed class ProdutoDbContext : DbContext, IUnitOfWork
+{
+    public ProdutoDbContext(DbContextOptions<ProdutoDbContext> options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+
+    public async Task<bool> Commit()
+    {
+        // TODO: Aqui você pode percorrer os eventos de dominio e processa-los
+        return await SaveChangesAsync() > 0;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                     .SelectMany(e => e.GetForeignKeys()))
+            relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProdutoDbContext).Assembly);
+        modelBuilder.Ignore<Event>();
+        modelBuilder.Ignore<ValidationResult>();
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/Services/EF.Produtos.Infra/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,47 @@
+﻿using EF.Domain.Commons.Repository;
+using EF.Produtos.Domain.Models;
+using EF.Produtos.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Produtos.Infra.Data.Repository;
+
+public sealed class ProdutoRepository : IProdutoRepository
+{
+    private readonly ProdutoDbContext _dbContext;
+
+    public ProdutoRepository(ProdutoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IUnitOfWork UnitOfWork => _dbContext;
+
+    public async Task<Produto> Criar(Produto produto, CancellationToken cancellationToken)
+    {
+        var result = await _dbContext.Produtos.AddAsync(produto, cancellationToken);
+        return result.Entity;
+    }
+
+    public Produto Atualizar(Produto produto, CancellationToken cancellationToken)
+    {
+        var result =  _dbContext.Produtos.Update(produto);
+        return result.Entity;
+    }
+
+    public async Task<IList<Produto>> Buscar(ProdutoCategoria? categoria, CancellationToken cancellationToken)
+    {
+       return await _dbContext.Produtos
+            .Where(produto => produto.Ativo && (categoria == null || produto.Categoria == categoria))
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Produto?> BuscarPorId(Guid produtoId, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Produtos.SingleAsync(produto => produto.Id == produtoId, cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+    }
+}

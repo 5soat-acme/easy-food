70c9f1907f80048ed034ff648389628f2b183978
Pedro Barão <pedrobarao@users.noreply.github.com>
2024-01-28 08:09:30 -0300

    incluindo tempo de espera no monitor

--- src/Services/EF.PreparoEntrega.Application/Mapping/DomainToDtoProfile.cs
+++ src/Services/EF.PreparoEntrega.Application/Mapping/DomainToDtoProfile.cs
@@ -8,8 +8,17 @@ public class DomainToDtoProfile : Profile
 {
     public DomainToDtoProfile()
     {
-        CreateMap<Pedido, PedidoMonitorDto>();
+        CreateMap<Pedido, PedidoMonitorDto>()
+            .ForMember(dest => dest.TempoEspera, opt => opt.MapFrom(src => FormatTempoDecorrido(src.DataCriacao)));
         CreateMap<Pedido, PedidoPreparoDto>();
         CreateMap<Item, ItemPreparoDto>();
+    }
+
+    private string FormatTempoDecorrido(DateTime dataCriacao)
+    {
+        DateTime agoraUtc = DateTime.UtcNow;
+        TimeSpan tempoDecorrido = agoraUtc - dataCriacao;
+        tempoDecorrido = tempoDecorrido.Duration();
+        return $"{tempoDecorrido.Hours:D2}:{tempoDecorrido.Minutes:D2}:{tempoDecorrido.Seconds:D2}";
     }
 }

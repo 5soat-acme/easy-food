74125b525a24096c89ed42440d6aaf9223630cab
Pedro Barão Filgueiras <pedrobarao@users.noreply.github.com>
2024-03-01 10:43:48 -0300

    ajustando identidade

--- test/EF.Commons.Test/Helpers/ProdutoHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace EF.Commons.Test.Helpers;
-
-public class ProdutoHelper
-{
-    public Guid Id { get; set; }
-    public decimal ValorUnitario { get; set; }
-    public string Descricao { get; set; }
-    public string TempoPreparo { get; set; }
-}
